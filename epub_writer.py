fbf57ed1a3857d1ea91a478aeeb7f9de3d023924
mizzusano <mizzu@localhost.localdomain>
2010-03-26 18:32:41 +0900

    update

--- epub_writer.py
+++ epub_writer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import sys , os , simplejson
+import sys , os , tempfile , shutil , zipfile , re
 from optparse import OptionParser
+
 import models
 
 def main():
@@ -16,12 +17,90 @@ def main():
     ( filename , images ) = open_directory( options.dir )
     if not images :
         parser.error("directory is not open . directory not exists 'images' directory ")
+
+    tmpdir = tempfile.mkdtemp()
+    os.makedirs( os.path.join(tmpdir , "OEBPS") )
+    os.makedirs( os.path.join(tmpdir , "OEBPS" , "images")  )
+    os.makedirs( os.path.join(tmpdir , "OEBPS", "text")  )
+    os.makedirs( os.path.join(tmpdir ,"META-INF") )
+    
+    create_mimetype(outdir=tmpdir,)
+    create_container(outdir=os.path.join( tmpdir , "META-INF") )
+    images = copy_images(images=images,outdir=os.path.join( tmpdir , "OEBPS" , "images") )
+    contents = create_text_contents(images=images,outdir=os.path.join( tmpdir , "OEBPS" , "text" ) )
+    create_ncx(filename=filename , images=images , outdir=os.path.join( tmpdir , "OEBPS") ,contents=contents )
+    create_opf(filename=filename,images=images,contents=contents,outdir=os.path.join(tmpdir , "OEBPS") )
+    dst_path = create_zip(filename=filename,tmpdir=tmpdir,outdir="/home/mizzu/tmp/")
+
+    os.rename( dst_path , re.compile("\.zip$").sub( ".epub" , dst_path )  )
+
+    rmdirs = []
+    for root,dirs,files in os.walk( tmpdir ):
+        for file in files :
+            os.remove( os.path.join( root , file ) )
+        rmdirs.append( os.path.join( root ,file ) )
+
+    rmdirs.reverse()
+    for dir in rmdirs :
+        os.rmdir( "/".join( [ str(x) for x in dir.split("/")[0:-1] ] ) )
+
+def create_zip(*args,**kwargs):
+    path = kwargs.get("outdir") + kwargs.get("filename") + ".zip"
+    zip = zipfile.ZipFile( path  ,"w" , zipfile.ZIP_DEFLATED )
+    reg = None
+    for root,dirs,files in os.walk( kwargs.get("tmpdir") ):
+        if not reg : reg = re.compile( str( root )  ) 
+        for file in files :
+            relative = re.compile("^/").sub("" , reg.sub("" , root ) )
+            zip.write( os.path.join(root,file) , os.path.join( relative , file ) )
+    zip.close()
+    return path
+
+
+def create_opf(*args,**kwargs):
+    opfDocument = models.OpfDocument(kwargs)
+    f = open( os.path.join( kwargs.get("outdir") , opfDocument.filename ) , "w")
+    f.write( opfDocument.doc.toxml() )
+    f.close()
+
+def create_text_contents(*args,**kwargs):
+    contents = []
+    i = 1
+    for image in kwargs.get("images"):
+        contentXHTML = models.ContentXHtml(i , image.split("/").pop() )
+        i+=1
         
-    create_ncx(filename=filename , images=images)
+        f = open( os.path.join(  kwargs.get("outdir") , contentXHTML.filename ) , "w")
+        f.write( contentXHTML.doc.toxml() )
+        f.close()
+        contents.append( contentXHTML.filename )
+    return contents
+
+def copy_images(*args,**kwargs):
+    images = []
+    for img in kwargs.get("images"):
+        fname = img.split("/").pop()
+        shutil.copyfile( img , os.path.join( kwargs.get("outdir") , fname)  )
+        images.append( kwargs.get("outdir") + "/" + fname )
+
+    return images
+
+def create_container(*args,**kwargs):
+    containerDocument = models.ContainerDocument()
+    f = open( os.path.join( kwargs.get("outdir") ,  containerDocument.filename ) , "w")
+    f.write( containerDocument.doc.toxml() )
+    f.close()
+
+def create_mimetype(*args,**kwargs):
+    f = open( os.path.join( kwargs.get("outdir") , "mimetype" ) , "w" )
+    f.write("application/epub+zip")
+    f.close()
 
 def create_ncx(*args,**kwargs):
-    hoge = models.NCXDocument(kwargs)
-
+    ncxDocument = models.NCXDocument(kwargs)
+    f = open( os.path.join( kwargs.get("outdir") , ncxDocument.filename ) , "w")
+    f.write( ncxDocument.doc.toxml() )
+    f.close()
 
 def open_directory(path):
     images = []
@@ -38,7 +117,7 @@ def open_directory(path):
         for root,dirs,files in os.walk( path + "/images"):
             if not len(files) > 0 : continue
             for file in files :
-                images.append( file )
+                images.append( root + "/" + file )
     return filename , images
 
 
--- models.py
+++ models.py
@@ -1,11 +1,143 @@
 from xml.dom.minidom import getDOMImplementation
+from getimageinfo import getImageInfo
 
 class DocumentRoot():
     def __init__(self):
         self.impl = getDOMImplementation()
+
+class ContentXHtml(DocumentRoot):
+    def __init__(self,i,image):
+        self.filename = "contents%s.xhtml" % ( str(i).zfill(5)  )
+        super = DocumentRoot()
+        doctype = super.impl.createDocumentType("html" , "-//W3C//DTD XHTML 1.0 Strict//EN" , "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" )
+        doc = super.impl.createDocument("html","html",doctype)
+        html = doc.documentElement
+        html.setAttribute("xmlns" , "http://www.w3.org/1999/xhtml")
+        head = doc.createElement("head")
+        html.appendChild( head )
+
+        body = doc.createElement("body")
+        div = doc.createElement("div")
+        h1 = doc.createElement("h1")
+        h1.setAttribute("title" , "title")
+        h1.setAttribute("id" , "page%s" % ( i ) )
+        img = doc.createElement("img")
+        img.setAttribute("src" , "../images/%s" % ( image ) )
+        h1.appendChild( img )
+        div.appendChild( h1 )
+        body.appendChild( div )
+        html.appendChild( body )
+
+        self.doc = doc
+
+        
+class OpfDocument(DocumentRoot):
+    filename = "content.opf"
+    
+    def __init__(self,kwargs):
+        super = DocumentRoot()
+        doc = super.impl.createDocument("package","package",None)
+        package = doc.documentElement
+        package.setAttribute("xmlns" , "http://www.idpf.org/2007/opf")
+        package.setAttribute("unique-identifier" , "BookID")
+        package.setAttribute("version" , "2.0")
+        metadata = doc.createElement("metadata")
+        metadata.setAttribute("xmlns:dc" , "http://purl.org/dc/elements/1.1/")
+        metadata.setAttribute("xmlns:opf" , "http://www.idpf.org/2007/opf")
+        package.appendChild( metadata ) 
+        dc = doc.createElement("dc:title")
+        dc.appendChild( doc.createTextNode( kwargs.get("filename") ) )
+        metadata.appendChild(dc)
+        if kwargs.get("creator"):
+            dc = doc.createElement("dc:creator")
+            dc.setAttribute("opf:role" , "aut")
+            dc.appendChild( doc.createTextNode( kwargs.get("creator") ) )
+            metadata.appendChild(dc)
+
+        if kwargs.get("language"):
+            dc = doc.createElement("dc:language")
+            dc.appendChild( doc.createTextNode( kwargs.get("language") ) )
+            metadata.appendChild(dc)
+
+        if kwargs.get("identifier"):
+            dc = doc.createElement("dc:identifier")
+            dc.setAttribute("id" , "BookID")
+            dc.setAttribute("opf:scheme" , "UUID" )
+            dc.appendChild( doc.createTextNode( kwargs.get("identifier") ) )
+            metadata.appendChild(dc)
+
+        meta = doc.createElement("meta")
+        meta.setAttribute("name" , "Sigil Version")
+        meta.setAttribute("content","0.1.8")
+        metadata.appendChild( meta )
+
+        manifest = doc.createElement("manifest")
+        item = doc.createElement("item")
+        item.setAttribute("id" , "ncx")
+        item.setAttribute("href" , "toc.ncx")
+        item.setAttribute("media-type" , "application/x-dtbncx+xml")
+        manifest.appendChild( item )
+
+        for image in kwargs.get("images") :
+            info = getImageInfo( open(image,"r").read() ) 
+            filename = image.split("/").pop()
+            item = doc.createElement("item")
+            item.setAttribute("id" , filename)
+            item.setAttribute("href" , "images/" + filename )
+            item.setAttribute("media-type" , info[0] )
+            manifest.appendChild( item )
+
+
+        for content in kwargs.get("contents") :
+            item = doc.createElement("item")
+            item.setAttribute("id" , content )
+            item.setAttribute("href" , "text/" + content )
+            item.setAttribute("media-type" , "application/xhtml+xml" )
+            manifest.appendChild( item )
+
+        package.appendChild( manifest ) 
+
+        spine = doc.createElement("spine")
+        spine.setAttribute("toc" , "ncx")
+
+        for content in kwargs.get("contents") :
+            itemref = doc.createElement("itemref")
+            itemref.setAttribute("idref" , content )
+            spine.appendChild( itemref )
+
+        package.appendChild( spine )
+
+        guide = doc.createElement("guide")
+        reference = doc.createElement("reference")
+        reference.setAttribute("type" , "cover")
+        reference.setAttribute("title" , kwargs.get("filename") )
+        reference.setAttribute("href" , "text/" + kwargs.get("contents").pop(0) )
+        guide.appendChild( reference )
+        package.appendChild( guide )
+        self.doc = doc
+
+
+class ContainerDocument(DocumentRoot):
+    filename = "container.xml"
+
+    def __init__(self):
+        super = DocumentRoot()
+        doc = super.impl.createDocument("container","container",None)
+        container = doc.documentElement
+        container.setAttribute("version" , "1.0")
+        container.setAttribute("xmlns" , "urn:oasis:names:tc:opendocument:xmlns:container")
+        rootfiles = doc.createElement("rootfiles")
+        rootfile = doc.createElement("rootfile")
+        rootfile.setAttribute("full-path" , "OEBPS/content.opf")
+        rootfile.setAttribute("media-type" , "application/oebps-package+xml")
+        rootfiles.appendChild( rootfile )
+        container.appendChild( rootfiles ) 
+        self.doc = doc
+
         
 class NCXDocument(DocumentRoot):
     filename = "toc.ncx"
+
     def __init__(self,kwargs):
         super = DocumentRoot()
         doctype = super.impl.createDocumentType("ncx" , "-//NISO//DTD ncx 2005-1//EN" , "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd" )
@@ -30,11 +162,31 @@ class NCXDocument(DocumentRoot):
         meta.setAttribute("name" , "dtb:maxPageNumber")
         meta.setAttribute("content" , "0")
         head.appendChild(meta)
+        root.appendChild( head )
+
         docTitle = doc.createElement("docTitle")
         text = doc.createElement("text")
         text.appendChild( doc.createTextNode( kwargs.get("filename") ) )
         docTitle.appendChild( text ) 
-        root.appendChild( head )
         root.appendChild( docTitle )
+        
+        navMap = doc.createElement("navMap")
+        i = 1
+        #for imgsrc in kwargs.get("images"):
+        for content in kwargs.get("contents") :
+            navPoint = doc.createElement("navPoint")
+            navPoint.setAttribute("id" , "navPoint-%s" % ( i ) )
+            navPoint.setAttribute("playOrder" , "%s" % ( i ) )
+            navLabel = doc.createElement("navLabel")
+            text = doc.createElement("text")
+            text.appendChild( doc.createTextNode( "page%s" % (i)  ) )
+            navLabel.appendChild( text ) 
+            navPoint.appendChild( navLabel )
+            cont = doc.createElement("content")
+            cont.setAttribute("src" , "text/" + content ) 
+            navPoint.appendChild( cont ) 
+            navMap.appendChild( navPoint )
+            i+=1
+        root.appendChild( navMap )
 
-        print doc.toxml()
+        self.doc = doc
